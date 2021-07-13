a1fa631e3bbc34926a94679a90060446298ab775
MAZHAR <iiestshibpur605@gmail.com>
2021-07-13 14:09:12 +0530

    Improved solution

--- Find Peak Element.cpp
+++ Find Peak Element.cpp
@@ -1,3 +1,4 @@
+//Approach-1 (Simple binary search)
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
@@ -11,6 +12,54 @@ public:
                 l = mid+1;
             else
                 r = mid;
+        }
+        return l;
+    }
+};
+
+//Approach-2 (More intuitive)
+class Solution {
+public:
+    int findPeakElement(vector<int> &A) {
+        int n = A.size();
+        if(n == 1)
+            return 0;
+        
+        if(A[0] > A[1]) return 0;
+        if(A[n-1] > A[n-2]) return n-1;
+
+        int l = 1, r = n-2; //since, we already checked for 0 and (n-1) indices above
+        int mid;
+        
+        /*
+            case-1:
+              mid(peak)
+                /\
+            .../  \...
+            
+            
+            case-2:
+              (peak)
+                /\
+            .../  \mid...
+            
+            
+            case-3:
+                 (peak)
+                   /\
+            ...mid/  \...
+        */
+        
+        while(l < r) {
+            mid = l + (r-l)/2;
+            
+            if(A[mid] > A[mid-1] && A[mid] > A[mid+1]) //1. mid is PEAK
+                return mid;
+            
+            if(A[mid] < A[mid-1])      //2. mid is right to PEAK. So, go towards left
+                r = mid-1;
+            else if(A[mid] < A[mid+1]) //3. mid is left to PEAK. So, go towards right
+                l = mid+1;
         }
         return l;
     }
